f8454c87061c715d50ffa2ea055ec80d67ccbb8d
Gabriel Dos Santos Figueiredo <gabrieldossantosfigueiredo@gmail.com>
2024-01-30 14:31:11 -0300

    Add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainPageComponent } from "./main-page/main-page.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, MainPageComponent, NavbarComponent]
 })
 export class AppComponent {
   title = 'projetoJoji';
