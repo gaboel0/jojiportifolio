1ec1a37ee9a35412153d4b3c5366145d854dc616
G389127- <gabrieldossantosfigueiredo@gmail.com>
2024-02-05 16:00:02 -0300

    fix:Navbar and main page fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainPageComponent } from "./main-page/main-page.component";
 import { NavbarComponent } from "./navbar/navbar.component";
+import { CarrousselComponent } from './carroussel/carroussel.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, MainPageComponent, NavbarComponent]
+    imports: [RouterOutlet, MainPageComponent, NavbarComponent, CarrousselComponent]
 })
 export class AppComponent {
   title = 'projetoJoji';
---
+++ src/app/carroussel/carroussel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carroussel',
+  standalone: true,
+  imports: [],
+  templateUrl: './carroussel.component.html',
+  styleUrl: './carroussel.component.scss'
+})
+export class CarrousselComponent {
+
+}
